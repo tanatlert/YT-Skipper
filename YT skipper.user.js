5b6b22ab11ebb5fcf368b55296cc69c9e036b2c8
tanatlert <55281221+tanatlert@users.noreply.github.com>
2022-10-20 13:12:47 +0700

    Update YT skipper.user.js

--- YT skipper.user.js
+++ YT skipper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         !YT skipper
 // @namespace    https://github.com/tanatlert/YT-Skipper
-// @version      4.1.2
+// @version      4.1.3
 // @description  Multi-function YouTube Ad-Skipper
 // @author       coder369
 // @match        *://*.youtube.com/*
@@ -21,16 +21,16 @@ setInterval(function(){
     if(document.getElementsByClassName("style-scope ytd-mealbar-promo-renderer style-text size-default").length > 0){ //remove promotion
         document.getElementsByClassName("style-scope ytd-mealbar-promo-renderer style-text size-default")[0].click();
     }
-    //if(document.getElementsByClassName("ytp-ad-preview-container").length > 0){
-    //    document.getElementsByClassName("ytp-ad-preview-container")[0].remove();
-    //}
-    if(document.getElementsByClassName('style-scope ytd-player-legacy-desktop-watch-ads-renderer').length > 0){ //remove panel ads
-        document.getElementsByClassName('style-scope ytd-player-legacy-desktop-watch-ads-renderer')[0].remove()
+    if(document.getElementsByClassName("style-scope yt-mealbar-promo-renderer").length > 0){
+        document.getElementsByClassName("style-scope yt-mealbar-promo-renderer")[0].remove();
     }
+    //if(document.getElementsByClassName('style-scope ytd-player-legacy-desktop-watch-ads-renderer').length > 0){ //remove panel ads
+    //     document.getElementsByClassName('style-scope ytd-popup-container')[0].remove();
+//}
     if((document.getElementsByClassName("ytp-ad-player-overlay-skip-or-preview").length > 0) || (document.getElementsByClassName("ytp-ad-preview-container").length > 0) || (document.getElementsByClassName("ytp-ad-text ytp-ad-preview-text").length > 0)){ //mute and speed up ads
         clickMute();
         var v = document.querySelector('video'); var t = 16; v.playbackRate = parseFloat(t);
-        setTimeout(function() {clickMute()}, 800);
+        setTimeout(function() {clickMute()}, 1000);
     }
      if(document.getElementsByClassName('style-scope ytd-item-section-renderer sparkles-light-cta').length > 0){ //remove panel ads2
         document.getElementsByClassName('style-scope ytd-item-section-renderer sparkles-light-cta')[0].remove()
